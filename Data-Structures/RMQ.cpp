f2d353959537380ef9c64d1295c4f0857b85fc9b
nk17kumar <nk17kumar@gmail.com>
2017-02-20 14:38:41 +0530

    segtree implementation

---
+++ Data-Structures/RMQ.cpp
@@ -0,0 +1,60 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int a[100001];
+int segtree[100001*3];
+
+void build(int node,int low,int high)
+{
+	if(low == high)
+	{
+	 segtree[node] = a[low];
+	 return;
+	}
+	int mid = (low+high)>>1;
+	update(2*node,low,mid);
+	update(2*node+1,mid+1,high);
+	segtree[node]=min(segtree[2*node],segtree[2*node+1]);
+}
+
+void update(int node,int low,int high,int idx,long long val)
+{
+	if(low>high)
+		return;
+	if(low == high)
+	{
+		segtree[node] = val;
+		return;
+	}
+	int mid = (low+high)>>1;
+	 if(low<=idx&& idx<=mid)
+	update(2*node,low,mid,idx,val);
+    else
+	update(2*node+1,mid+1,high,idx,val);
+
+	segtree[node]=min(segtree[2*node],segtree[2*node+1]);
+}
+
+ll query(int node,int low,int high,int l,int r)
+{
+	if(l<=low && r>=high)
+	  {
+	     return segtree[node];
+	  }  	
+	       int mid=(low+high)>>1;
+	        if(r<=mid)
+			return query(2*node,low,mid,l,r);
+		   else if(l>mid)
+			return query(2*node+1,mid+1,high,l,r);
+ 
+      int p1=query(2*node,low,mid,l,r);
+      int p2=query(2*node+1,mid+1,high,l,r);
+      return min(p1,p2);
+}
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
