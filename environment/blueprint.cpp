0559e84ce4925ce19e7b2a14e1fdc4c5696c9f12
nk17kumar <nk17kumar@gmail.com>
2018-02-09 19:00:04 +0530

    modified the prototype

--- environment/blueprint.cpp
+++ environment/blueprint.cpp
@@ -1,64 +1,40 @@
-/*     _    _ _____ _                               
- _ __ | | _/ |___  | | ___   _ _ __ ___   __ _ _ __ 
-| '_ \| |/ / |  / /| |/ / | | | '_ ` _ \ / _` | '__|
-| | | |   /| | / / |   /| |_| | | | | | | (_| | |   
-|_| |_|_|\_\_|/_/  |_|\_\\__,_|_| |_| |_|\__,_|_|   
-
- #my awesome codes ;)
-*/
-  
+// with <3 
+// by nk17kumar ;)   
 #include "bits/stdc++.h"
 using namespace std;
  
-// my competitive tools
-
-#define rep(i,a,b)                for(int i=a;i<b;++i)
-#define per(i,a,b)                for(int i=a;i>=b;--i)
-#define each(it,a)                for(auto it = a.begin();it != a.end();++it)
-#define all(a)                    (a.begin(),a.end())
-#define fill(a)                   memset(a,0,sizeof(a))
-#define arrin(a,size)             for(int i=0;i<size;++i) cin >> a[i];
-#define print(x)                  printf("%lld\n",(long long)x);
-#define fastinput()               ios_base :: sync_with_stdio(false);
-#define rocknroll()               int t; cin >> t; while(t--) 
-#define input()                   freopen("input.txt","r",stdin)
-#define output()                  freopen("output.txt","w",stdout)
-#define inchar                    getchar_unlocked
-#define sz(a)                     (int)a.size()
-#define pb                        push_back
-#define mp                        make_pair
-#define ull                       unsigned long long
-#define ll                        long long
-#define mod                       1000000007
-#define inf                       INT_MAX
-#define inff                      LLONG_MAX
-#define X                         first
-#define Y                         second
-/*-----------------------------------------------------------------------------------------------------------------------------------------------*/ 
-double tick_tock(){ static clock_t oldtick;clock_t newtick=clock();
-                  double diff = 1.0*(newtick-oldtick)/CLOCKS_PER_SEC;
-                  oldtick = newtick;return diff;
-              }
-template <typename T> void inPos(T &x)
-              {     x=0;register T c=inchar();while(((c<48)||(c>57))&&(c!='-'))c=inchar();
-                    bool neg=false;if(c=='-')neg=true;for(;c<48||c>57;c=inchar());
-                    for(;c>47&&c<58;c=inchar())x=(x<<3)+(x<<1)+(c&15);if(neg)x=-x;
-                }
-/*----------------------------------------------------------------------------------------------------------------------------------------------*/
-
-
-int main(int argc, char const *argv[])
-{   
-  fastinput();
-    rocknroll()
-    { 
-      
-
-    
-
-    }
-  //printf("%lf\n",tick_tock());
-  return 0;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector <pii> vpii;
+typedef vector <pll> vpll;
+typedef vector <ll> vll;
+typedef vector <int> vi;
+ 
+#define _ auto
+#define X first
+#define Y second
+#define pb push_back
+#define mp make_pair
+ 
+const ll mod = 1e9+7;
+const int inf = mod;
+const ll inff = 1LL<<62;
+ 
+#define fi() ios_base::sync_with_stdio(0)
+#define ou() freopen("out.txt","w",stdout)
+#define in() freopen("in.txt","r",stdin)
+#define rep(i,a,b) for(int i=a;i<b ;i++)
+#define per(i,a,b) for(int i=a;i>=b ;i--)
+#define vin(v,n) rep(i,0,n) cin>>v[i];
+#define sz(v) (int)v.size()
+#define all(v) v.begin(),v.end()
+ 
+ 
+int main(int argc, char const *argv[]) { 
+  fi(); //in();ou();
+  ${1:code}  
+ 
+    return 0;
 }
-
-
+ 
